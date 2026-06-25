f72bf6ff2ec3517b92daa13c88f9fbbe6939c3bd
Vishnu-Hack <vishnu.406.39965@gmail.com>
2026-06-25 11:13:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler'; // <-- Moved to the very top
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { RatesProvider } from './RatesContext';
+import { RatesProvider } from './components/RatesContext';
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
